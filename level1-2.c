0a2c0b12d3adc6b48ccb738f960d59379e1b4c14
FZH <1976958279@qq.com>
2023-09-11 12:42:09 +0800

    refresh 1

--- level1-2.c
+++ level1-2.c
@@ -6,16 +6,17 @@ int main()
     fin=fopen("chart.txt","rb");
     fout=fopen("result.txt","wb");
     int now,elv,goal;
-    scanf("elevator %d\n%d %d",&elv,&now,&goal);
+    fscanf(fin,"elevator %d\n%d %d",&elv,&now,&goal);
     if(fin==NULL)printf("ERROR!");    //判断文件是否读取成功
     else
     {
         int time=0;
-        printf("%d 0 0\n",elv);
+        fprintf(fout,"%d 0 0\n",elv);
         time+=abs(now-elv);     //time的计算：模拟运行过程的计时环节
-        printf("%d %d 1\n",now,time);
+        fprintf(fout,"%d %d 1\n",now,time);
         time+=abs(now-goal);
-        printf("%d %d 0\n",goal,time);
+        fprintf(fout,"%d %d 0\n",goal,time);
     }
+    fclose(fin);fclose(fout);
     return 0;
 }
