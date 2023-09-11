29fb0cec7b96262ab398b9a0561dce210916a931
FZH <1976958279@qq.com>
2023-09-11 13:58:01 +0800

    provide another way to solve level1-2

---
+++ level1-2v2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    FILE *fin=NULL,*fout=NULL;
+    fin=fopen("chart.txt","rb");
+    fout=fopen("result.txt","wb");
+    int now,elv,goal;
+    fscanf(fin,"elevator %d\n%d %d",&elv,&now,&goal);
+    if(fin==NULL)printf("ERROR!");               //判断是否读取成功
+    else{
+        int time=0;
+        fprintf(fout,"%d 0 0\n",elv);
+        time+=abs(now-elv);     
+        fprintf(fout,"%d %d 1\n",now,time);
+        time+=abs(now-goal);
+        fprintf(fout,"%d %d 0\n",goal,time);
+    }
+    fclose(fin);fclose(fout);
+    return 0;
+}
